8286e121bac32b559c79b6e133f16456821eacdc
Angestykhwan <anggieangestifitri@gmail.com>
2022-11-15 17:09:13 +0700

    MOBILE_ALARM-MANAGER

---
+++ app/src/main/java/com/example/bab6_alarmmanager/DestinationActivity.java
@@ -0,0 +1,14 @@
+package com.example.bab6_alarmmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DestinationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_destination);
+    }
+}
